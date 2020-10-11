f3c3985cdb8b002c6c4d6cb01943ba7029a2a03b
Mutlu Can ATES <canates94@gmail.com>
2020-10-11 23:59:17 +0300

    get followed rooms

--- src/services/rooms/rooms.class.js
+++ src/services/rooms/rooms.class.js
@@ -2,18 +2,22 @@ const { Service } = require('feathers-mongoose');
 
 exports.Rooms = class Rooms extends Service {
   async find(params) {
-    return [];
+    return super.find({
+      query: {
+        _id: {
+          $in: [...params.user.followedGroups],
+        },
+      },
+    });
   }
   async get(id, params) {}
   async create(data, params) {
-   
-
     return super.create({
-          title: data.title,
-          private: data.private,
-          founder: data.founder,
-          ticket: data.ticket ? data.ticket : null
-      })
+      title: data.title,
+      private: data.private,
+      founder: data.founder,
+      ticket: data.ticket ? data.ticket : null,
+    });
   }
   async update(id, data, params) {}
   async patch(id, data, params) {}
